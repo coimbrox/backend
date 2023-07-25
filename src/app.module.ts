444c7a824e0c0bab2b2730cff29605e349dc9b56
Gabriel Coimbra De Oliveira Da Silva <54251679+coimbrox@users.noreply.github.com>
2023-07-25 15:25:02 -0300

    Configurando Validacoes Firebase

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ThingsModule } from './things/things.module';
+import { AuthModule } from './auth/auth.module';
 import { CategoriesModule } from './categories/categories.module';
 import { CommonModule } from './common/common.module';
-import { FirebaseService } from './auth/firebase/firebase.service';
-import { AuthModule } from './auth/auth.module';
+import { ThingsModule } from './things/things.module';
 
 @Module({
-  imports: [ThingsModule, CategoriesModule, CommonModule, AuthModule],
+  imports: [AuthModule, ThingsModule, CategoriesModule, CommonModule],
   controllers: [AppController],
-  providers: [AppService, FirebaseService],
+  providers: [AppService],
 })
 export class AppModule {}
